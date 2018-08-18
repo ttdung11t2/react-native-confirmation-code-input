f1f25fa2d83c23ff3b9deea2c51f2a6de4685517
David Narbutovich <vamnemne@gmail.com>
2018-08-18 16:55:33 +0300

    Add example

---
+++ examples/rn56/clone.js
@@ -0,0 +1,20 @@
+const { resolve } = require('path');
+
+const fs = require('fs-extra');
+
+const OUT_PATH = resolve(
+  __dirname,
+  'node_modules/react-native-confirmation-code-field/',
+);
+
+fs.ensureDirSync(resolve(OUT_PATH, './src/'));
+
+fs.copySync(
+  resolve(__dirname, '../../index.js'),
+  resolve(OUT_PATH, './index.js'),
+);
+fs.copySync(
+  resolve(__dirname, '../../package.json'),
+  resolve(OUT_PATH, './package.json'),
+);
+fs.copySync(resolve(__dirname, '../../src'), resolve(OUT_PATH, './src'));
---
+++ examples/rn56/styles.js
@@ -0,0 +1,54 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F6CE',
+  },
+  titleWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  title: {
+    color: 'red',
+    fontSize: 16,
+    fontWeight: '800',
+    paddingVertical: 30,
+  },
+  wrapper: {
+    marginTop: 30,
+  },
+  inputWrapper1: {
+    paddingVertical: 50,
+    paddingHorizontal: 20,
+    backgroundColor: '#009C92',
+  },
+  inputWrapper2: {
+    paddingVertical: 50,
+    paddingHorizontal: 20,
+    backgroundColor: '#E0F8F1',
+  },
+  inputWrapper3: {
+    paddingVertical: 50,
+    paddingHorizontal: 20,
+    backgroundColor: '#2F0B3A',
+  },
+  inputLabel1: {
+    color: '#fff',
+    fontSize: 14,
+    fontWeight: '800',
+  },
+  inputLabel2: {
+    color: '#31B404',
+    fontSize: 14,
+    fontWeight: '800',
+    textAlign: 'center',
+  },
+  inputLabel3: {
+    color: '#fff',
+    fontSize: 14,
+    fontWeight: '800',
+    textAlign: 'center',
+  },
+});
